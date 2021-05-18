e42878d325be6a411e741c681ab68db2edd08bec
thallesaguiar <thalles-aguiar@hotmail.com>
2021-05-18 01:33:58 -0300

    inital commit

---
+++ backend/classes/UsuarioVO.php
@@ -0,0 +1,17 @@
+<?php
+class UsuarioVO{
+    private $id_usuario;
+    private $nome;
+    private $sobrenome;
+    private $email;
+    private $empresario;
+    private $status;
+    private $senha;
+    private $data_nascimento;
+    private $genero;
+    private $telefone;
+    private $biografia;
+    private $id_empresario;
+    private $avatar;
+    private $coordenadas;
+}
---
+++ backend/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo json_encode(["Ok"=>"Bem vindo a API do Mateship"]);
