f7729cb6ed3d750354ca0b6cc27b0c5bb97ee059
thallesaguiar <thalles-aguiar@hotmail.com>
2021-06-10 04:21:55 -0300

    adicionado componente de mapbox

--- frontend/src/views/Map.vue
+++ frontend/src/views/Map.vue
@@ -1,13 +1,35 @@
 <template>
-  <p>map</p>
+  <MglMap :accessToken="accessToken" :mapStyle="mapStyle" />
 </template>
 
 <script>
-export default {
+import Mapbox from "mapbox-gl";
+import { MglMap } from "vue-mapbox";
 
-}
+export default {
+  components: {
+    MglMap,
+  },
+  data() {
+    return {
+      accessToken:
+        "pk.eyJ1Ijoic29jaWFsbWF0ZSIsImEiOiJja2NmZHFiMGEwMWQ5MndsaWVzaGxjMHB6In0.Lp8pyQ2bmdw9rnSzkEtTdA",
+      mapStyle: "mapbox://styles/mapbox/streets-v11",
+    };
+  },
+
+  created() {
+    this.mapbox = Mapbox;
+  },
+};
 </script>
 
 <style>
-
+.mgl-map-wrapper {
+  height: 100vh;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
 </style>
