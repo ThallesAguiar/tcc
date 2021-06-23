5bc9778e505c0e064d3dedee5e2f09b1ceccc9f8
Thalles Luz de Aguiar <thallesaguiar07@gmail.com>
2021-06-23 19:06:57 -0300

    adicionado marker ao mapa

--- frontend/src/views/Map.vue
+++ frontend/src/views/Map.vue
@@ -4,11 +4,22 @@
     <MglGeolocateControl
       position="top-right"
       :trackUserLocation="true"
-      :showUserLocation="true"
+      :showUserLocation="false"
     />
-    <MglPopup :coordinates="myCoordinates">
-      <span>Hello world!</span>
-    </MglPopup>
+    <MglMarker :coordinates="myCoordinates">
+      <div slot="marker">
+        <img
+          style="border-radius: 50%; width: 50px; height: 50px"
+          :src="user.avatar"
+        />
+      </div>
+
+      <MglPopup anchor="bottom">
+        <button type="button" class="btn btn-link" @click="visitProfile()">
+        {{ user.nome+' '+user.sobrenome }}
+        </button>
+      </MglPopup> 
+    </MglMarker>
   </MglMap>
 </template>
 
@@ -21,6 +32,7 @@ import {
   MglGeolocateControl,
   MglFullscreenControl,
   MglPopup,
+  MglMarker,
 } from "vue-mapbox";
 
 export default {
@@ -30,6 +42,7 @@ export default {
     MglGeolocateControl,
     MglFullscreenControl,
     MglPopup,
+    MglMarker,
   },
   data() {
     return {
@@ -37,7 +50,6 @@ export default {
         "pk.eyJ1Ijoic29jaWFsbWF0ZSIsImEiOiJja3E5bGdpcHEwNXMzMnBzMTZpeTRrZDk5In0.0ru5SvtMtBqp8gMH8Zh3vQ",
       mapStyle: "mapbox://styles/mapbox/streets-v11",
       myCoordinates: JSON.parse(localStorage.getItem("coordinates")),
-      // myCoordinates: [0, 0],
       user: {},
     };
   },
@@ -52,6 +64,8 @@ export default {
 
           if (!localStorage.getItem("coordinates"))
             localStorage.setItem("coordinates", JSON.stringify(coord));
+          console.log(coord);
+          this.myCoordinates = coord;
         },
         (err) => {
           console.log(err);
@@ -64,27 +78,32 @@ export default {
       );
     },
     async updateCoordinatesUser(id) {
-      // setInterval(function(){ alert("Hello"); }, 30000);
+      
       this.getCoordinatesCurrent();
 
-      // console.log(id);
-      // console.log(this.myCoordinates);
       try {
-        const coordsCurrent = await api.put(`coordinates/update.php`, {
-          id_user: id,
-          lat: this.myCoordinates[1],
-          lng: this.myCoordinates[0],
-        },{
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*",
-            "Authorization": localStorage.getItem("token")
+        const coordsCurrent = await api.put(
+          `coordinates/update.php`,
+          {
+            id_user: id,
+            lat: this.myCoordinates[1],
+            lng: this.myCoordinates[0],
+          },
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Headers": "*",
+              Authorization: localStorage.getItem("token"),
+            },
           }
-        });
-        console.log(coordsCurrent);
-        // localStorage.setItem("coordinates", coordsCurrent.data.coordinates);
+        );
+        console.log(coordsCurrent.data.coordenadas);
       } catch (error) {}
     },
+
+    visitProfile(){
+      console.log("visitar perfil");
+    }
   },
 
   computed: {
@@ -100,7 +119,29 @@ export default {
     this.user = JSON.parse(localStorage.getItem("user"));
 
     this.updateCoordinatesUser(this.user.id_usuario);
-    // this.getCoordinatesCurrent();
+    
+    setInterval(function () {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+
+          const coord = Object.values({ longitude, latitude });
+
+          if (!localStorage.getItem("coordinates"))
+            localStorage.setItem("coordinates", JSON.stringify(coord));
+          console.log(coord);
+          this.myCoordinates = coord;
+        },
+        (err) => {
+          console.log(err);
+        },
+        {
+          timeout: 30000,
+          enableHighAccuracy: true, //pega a localização via GPS
+          maximumAge: 1000, //como se fosse um cache, pra guardar a localização do GPS
+        }
+      );
+    }, 30000);
   },
 };
 </script>
